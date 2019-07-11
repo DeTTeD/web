183507c59dc050503193cfa31a122535bbf3a781
DeTTeD <37584038+DeTTeD@users.noreply.github.com>
2019-07-11 11:13:51 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo bash -c 'cat /home/box/web/etc/include.conf >> /etc/nginx/nginx.conf'
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo nginx -c /etc/nginx/siet-enabled/site.conf
+sudo nginx -c /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
