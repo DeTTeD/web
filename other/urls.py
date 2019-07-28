c4c1b4a0301aaf0032db694c2189ba1ec5082514
DeTTeD <37584038+DeTTeD@users.noreply.github.com>
2019-07-28 17:33:00 +0300

    Update urls.py

--- other/urls.py
+++ other/urls.py
@@ -6,11 +6,11 @@ from qa.views import test
 admin.autodiscover()
 
 
-urlpatterns = patterns('qa.views',
+urlpatterns = [
     url(r'^login/.*$',  'qa.views.test'),
     url(r'^$',  'qa.views.test'),
     url(r'^ask/.*',  'qa.views.test'),
     url(r'^popular/.*',  'qa.views.test'),
     url(r'^new/',  'qa.views.test'),
     url(r'^question/(?P<id>[0-9]+)/$',  'qa.views.test'),
-)
+]
