ed3e85816208630c794bb853d7d63323e5955354
DeTTeD <37584038+DeTTeD@users.noreply.github.com>
2019-07-28 14:06:44 +0300

    Create urls.py

---
+++ other/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import patterns, include, url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns = patterns('',
+    url(r'^login/', include('qa.urls')),
+    url(r'^$', include('qa.urls')),
+    url(r'^ask/', include('qa.urls')),
+    url(r'^popular/', include('qa.urls')),
+    url(r'^new/', include('qa.urls')),
+    url(r'^question/0.0.0.0/', include('qa.urls')),
+)
