5400760af3958f8b0d304101cbfe743c246ece66
DeTTeD <37584038+DeTTeD@users.noreply.github.com>
2019-07-28 13:07:58 +0300

    Update gunicorn.conf.py

--- etc/gunicorn.conf.py
+++ etc/gunicorn.conf.py
@@ -9,3 +9,14 @@ CONFIG = {
 		'hello:app',
 	),
 }
+CONFIG = {
+	'mode': 'wsgi',
+	'working_dir': '/home/box/web/ask/ask/',
+	'python': '/usr/bin/python',
+	'args': (
+		'--bind=0.0.0.0:8000',
+		'--workers=16',
+		'--timeout=60',
+		'wsgi:app',
+	),
+}
