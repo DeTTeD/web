030fb04280b15de28e2d9794455ba2183f0160b0
DeTTeD <37584038+DeTTeD@users.noreply.github.com>
2019-07-28 16:53:13 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
-sudo virtualenv -p python3 myvenv
-sudo source myvenv/bin/activate
-sudo pip install --upgrade pip
-sudo pip install django
-sudo pip install gunicorn
-sudo upgrade gunicorn
+sudo apt-get update
+sudo apt-get install -y python3.5
+sudo apt-get install -y python3.5-dev
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade django==2.1
+sudo pip3 install --upgrade gunicorn
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo nginx -c /etc/nginx/sites-enabled/test.conf
