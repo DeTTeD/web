e8d4518786d6ed3db1da4597537ec316b9527fe4
DeTTeD <37584038+DeTTeD@users.noreply.github.com>
2019-07-28 11:47:10 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ sudo nginx -c /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+sudo django-admin.py startproject ask
