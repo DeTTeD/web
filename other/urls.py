c9a923c05aafee017d41d5d6253c6dcaa5dc310d
DeTTeD <37584038+DeTTeD@users.noreply.github.com>
2019-07-29 06:07:23 +0300

    Update urls.py

--- other/urls.py
+++ other/urls.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url, include
 from qa.views import test
 
 admin.autodiscover()
 
 
 urlpatterns = [
-    url(r'^login/.*$',  'qa.views.test'),
-    url(r'^$',  'qa.views.test'),
-    url(r'^ask/.*',  'qa.views.test'),
-    url(r'^popular/.*',  'qa.views.test'),
-    url(r'^new/',  'qa.views.test'),
-    url(r'^question/(?P<id>[0-9]+)/$',  'qa.views.test'),
+    url(r'^login/.*$',  'qa.views.test', include('qa.urls')),
+    url(r'^$',  'qa.views.test', include('qa.urls')),
+    url(r'^ask/.*',  'qa.views.test', include('qa.urls')),
+    url(r'^popular/.*',  'qa.views.test', include('qa.urls')),
+    url(r'^new/',  'qa.views.test', include('qa.urls')),
+    url(r'^question/(?P<id>[0-9]+)/$',  'qa.views.test', include('qa.urls')),
 ]
