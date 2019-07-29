fbf21453993b28522eb63899cc46f789ff607936
DeTTeD <37584038+DeTTeD@users.noreply.github.com>
2019-07-29 07:13:57 +0300

    Update urls.py

--- other/urls.py
+++ other/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import patterns, include, url
-from qa.views import test, question, qa_list_all, qa_popular_all, ask_add, answer_add, signup_add, login_add
+
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-   url(r'^$', qa_list_all, name='main'),
-   url(r'^\?page=(?P<page>\d+)', qa_list_all, name='main'),
-   url(r'^login/', login_add, name='login'),
-   url(r'^signup/', signup_add, name='signup'),
-   url(r'^question/(?P<id>\d+)/', question, name='question'),
-   url(r'^ask/', ask_add, name='ask'),
-   url(r'^answer/', answer_add, name='answer'),
-   url(r'^popular/', qa_popular_all, name='popular'),
-   url(r'^popular/\?page=(?P<page>\d+)', qa_popular_all, name='popular'),
-   url(r'^new/', test, name='new'),
+    url(r'^$', 'qa.views.test'),
+    url(r'^login/$', 'qa.views.test'),
+    url(r'^signup/$', 'qa.views.test'),
+    url(r'^question/(\d+)$', 'qa.views.test'),
+    url(r'^ask/.*$', 'qa.views.test'),
+    url(r'^popular/$', 'qa.views.test'),
+    url(r'^new/$', 'qa.views.test'),
 )
