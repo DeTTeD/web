3d7b1a85f08f57411664e729e18095e19b817cc4
DeTTeD <37584038+DeTTeD@users.noreply.github.com>
2019-07-29 06:14:25 +0300

    Update urls.py

--- other/urls.py
+++ other/urls.py
@@ -7,10 +7,10 @@ admin.autodiscover()
 
 
 urlpatterns = [
-    url(r'^login/.*$',  'qa.views.test', include('qa.urls')),
-    url(r'^$',  'qa.views.test', include('qa.urls')),
-    url(r'^ask/.*',  'qa.views.test', include('qa.urls')),
-    url(r'^popular/.*',  'qa.views.test', include('qa.urls')),
-    url(r'^new/',  'qa.views.test', include('qa.urls')),
-    url(r'^question/(?P<id>[0-9]+)/$',  'qa.views.test', include('qa.urls')),
+    url(r'^login/.*$',  include('qa.urls')),
+    url(r'^$',  include('qa.urls')),
+    url(r'^ask/.*',  include('qa.urls')),
+    url(r'^popular/.*',  include('qa.urls')),
+    url(r'^new/',  include('qa.urls')),
+    url(r'^question/(?P<id>[0-9]+)/$',  include('qa.urls')),
 ]
